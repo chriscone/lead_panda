abaa6ee03a4d77252edda354b6afafc369217cd1
Chris Cone <jack_c_cone@homedepot.com>
2017-07-20 01:11:22 -0400

    Simplify init script by running a new command when cloning

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,30 +4,10 @@ OLD_DIR=""
 
 echo "start getting bootstrapped..."
 
-echo "creating new directory on the desktop"
-mkdir $NEW_DIR
-
-echo "moving files to the new directory"
-cp . $NEW_DIR
-
-pwd >> OLD_DIR
-
-echo "pwd"
-pwd
-
-echo "old dir"
-$OLD_DIR
-
-echo "navigate to new directory"
-cd  $NEW_DIR
-
-echo "clean up the git clone dir"
-#rm -R OLD_DIR
-
 echo "loading dependencies"
 npm install
 
-echo "executing test"
+echo "executing unit test"
 npm test
 
 echo "end bootstrapping"
