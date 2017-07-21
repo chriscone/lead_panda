95cb8e0eab88f0c1f425a3e911bacdefe0cc9a13
Chris Cone <jack_c_cone@homedepot.com>
2017-07-21 12:42:38 -0400

    Add working version of bowling game
    Additional test needed to cover other potential type of game, perfect game for example

--- app/Game.js
+++ app/Game.js
@@ -1,15 +1,33 @@
+var score;
+var rolls = [];
+var roll;
 
 var Game = function() {
-
+  score = 0;
+  roll = 0;
 };
 
 Game.prototype.roll = function(num) {
-
+  rolls[roll++] = num;
 };
 
-Game.prototype.score = function(num) {
+Game.prototype.score = function() {
+  var rollIndex = 0;
+  for (var i = 0; i < 10; i++) {
+    var pins = rolls[rollIndex];
 
+    if (pins == 10) {
+      score = score + 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+      rollIndex++;
+    }else if(pins + rolls[rollIndex + 1] == 10) {
+      score = score + 10 + rolls[rollIndex + 2];
+      rollIndex = rollIndex + 2;
+    }else {
+      score = score + pins + rolls[rollIndex + 1];
+      rollIndex = rollIndex + 2;
+    }
+  }
+  console.info("score: "+score);
+  return score;
 };
-
-
 module.exports = Game;
--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,4 +1,4 @@
-var Game = require("../app/dummy.js");
+var Game = require("../app/Game.js");
 describe("Game", function() {
   var game;
 
@@ -9,6 +9,54 @@ describe("Game", function() {
   
   it("should return a non null game", function() {
     expect(game).not.toBeNull();
+  });
+
+  it("should give a score of 20 when 1 pin is hit with each roll"
+    , function() {
+    var pins = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+    
+    pins.forEach(function (pin) {
+      game.roll(pin);
+    });
+    expect(game.score()).toEqual(20);
+  });
+
+  it("should give a score of 107"
+    , function() {
+    var pins = [4,2,10,8,2,6,4,3,1,8,2,5,4,2,7,1,5,7,2];
+    pins.forEach(function (pin) {
+      game.roll(pin);
+    });
+    expect(game.score()).toEqual(107);
+  });
+
+  it("should give a score of 73"
+    , function() {
+    var pins = [3,4,3,2,3,4,3,2,4,3,4,6,5,4,3,3,2,2,3,5];
+    pins.forEach(function (pin) {
+      game.roll(pin);
+    });
+    expect(game.score()).toEqual(73);
+  });
+
+  it("should give a score of 24"
+  , function() {
+  var pins = [1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1];
+  pins.forEach(function (pin) {
+    game.roll(pin);
+  });
+
+    expect(game.score()).toEqual(24);
+  });
+
+  it("should give a score of 72"
+  , function() {
+  var pins = [2,4,4,2,1,3,4,5,10,3,3,4,2,4,5,3,2,4,1];
+  pins.forEach(function (pin) {
+    game.roll(pin);
+  });
+
+    expect(game.score()).toEqual(72);
   });
 
 });
